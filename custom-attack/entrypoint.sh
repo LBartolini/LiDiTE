61b20a9957298058aaffadc2f91b72483704feaf
Lorenzo <l.bartolini02@gmail.com>
2024-08-05 18:13:10 +0200

    fix url DNS

--- custom-attack/entrypoint.sh
+++ custom-attack/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "nameserver 192.168.100.53" > /etc/resolv.conf
-echo "nameserver 127.0.0.13" >> /etc/resolv.conf
+echo "nameserver 127.0.0.11" >> /etc/resolv.conf
 wait4x tcp "${SCADA_HOST}:${SCADA_PORT}"
 echo "Scada Up"
 cd src
